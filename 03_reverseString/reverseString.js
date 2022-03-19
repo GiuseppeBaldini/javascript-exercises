2603ff529661c85ec9014648362c52569d454e4e
giuseppebaldini <giuseppebaldini@live.com>
2022-03-19 19:50:02 +0000

    Add reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let charsArray = str.split(''); // 'hello' => ['h', 'e', 'l', 'l', 'o']
+    let reversedChars = charsArray.reverse(); // => ['o', 'l', 'l', 'e', 'h']
+    let reversedString = reversedChars.join(''); // => 'olleh'
 
+    return reversedString;
 };
 
 // Do not edit below this line
